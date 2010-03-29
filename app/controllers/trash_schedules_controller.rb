39987279dce0345a789da23d36243b6199df029b
Willem van Bergen <willem@vanbergen.org>
2010-03-29 09:33:20 -0400

    Fixed a 500 error when searching with an empty string.

--- app/controllers/trash_schedules_controller.rb
+++ app/controllers/trash_schedules_controller.rb
@@ -1,28 +1,26 @@
 class TrashSchedulesController < ApplicationController
-  
+
   respond_to :html, :json
-  
+
   before_filter :load_trash_schedule, :only => :show
-  
+
   def index
-    if params[:q]
-
+    if !params[:q].blank?
        @trash_schedules = TrashSchedule.search(params[:q])
-
       if @trash_schedules.size == 1
-        redirect_to(trash_schedule_path(@trash_schedules.first)) 
+        redirect_to(trash_schedule_path(@trash_schedules.first))
       else
         respond_with(@trash_schedules)
       end
     end
   end
-  
+
   def show
     respond_with(@trash_schedule)
   end
-  
+
   protected
-  
+
   def load_trash_schedule
     @trash_schedule = TrashSchedule.find(params[:id])
   end
