349c2a52be9efce913a00a2f2c3f5163fa3ff666
Willem van Bergen <willem@vanbergen.org>
2012-10-19 13:20:10 +0200

    Redirect to ReCollect.

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,9 @@ OttawaTrash::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "trash_schedules#index"
+  # root :to => "trash_schedules#index"
+
+  match '/' => redirect('https://recollect.net/r/area/Ottawa')
 
   # See how all your routes lay out with "rake routes"
 
